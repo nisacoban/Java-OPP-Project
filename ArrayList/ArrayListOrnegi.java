0ad8706073483b04b589fac5358c5b0f6df6de21
nisacoban <hnisamaku@gmail.com>
2025-12-13 21:11:41 +0300

    Array List Project
    This code is about array list in opp more likely useful for learning

---
+++ ArrayList/ArrayListOrnegi.java
@@ -0,0 +1,34 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListOrnegi {
+    public static void main(String[] args) {
+
+        ArrayList k1= new ArrayList();
+        k1.add("Gökçe");
+        k1.add("İkra");
+        k1.add("Helen");
+        k1.add("Lara");
+        k1.add("Arven");
+        k1.add("Karen");
+        k1.add("İkra");
+        
+//        k1.forEach(System.out::println);
+        
+//        k1.remove("Lara");
+//        k1.forEach(System.out::println);
+
+//        System.out.println(k1.indexOf("İkra"));
+//        System.out.println(k1.lastIndexOf("İkra"));
+           
+//        k1.set(0, "Melike");
+//        k1.forEach(System.out::println);
+        
+
+//        k1.clear();
+//        k1.forEach(System.out::println);
+
+        System.out.println(k1.get(4));
+}
+}
