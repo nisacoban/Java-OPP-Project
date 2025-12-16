82021f4ddc7c6d27b0fecdce14585e18b476e0a8
nisacoban <hnisamaku@gmail.com>
2025-12-17 02:33:42 +0300

    Delete src directory

--- src/KiralikEv.java
+++
@@ -1,8 +0,0 @@
-public class KiralikEv extends Ev{
-    public KiralikEv(int metrekare) {
-        super.metrekare=metrekare;
-    }
-    public int getFiyat(){
-        return metrekare*50;
-    }
-}
--- src/SatilikEv.java
+++
@@ -1,8 +0,0 @@
-public class SatilikEv extends Ev{
-    public SatilikEv(int metrekare){
-        super.metrekare=metrekare;
-    }
-    public int getFiyat(){
-        return metrekare*500;
-    }
-}
