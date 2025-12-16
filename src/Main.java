e2a8e596aa934df7fb9dc446c70764a480e83420
nisacoban <hnisamaku@gmail.com>
2025-12-17 02:07:45 +0300

    Bank Account System in Java
    bank account system with using oop in java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,17 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner reader=new Scanner(System.in);
-        System.out.println("Dizinin eleman sayısını girin: ");
-        int n= reader.nextInt();
-        int[] dizi=new int[n];
-        int [] fr = new int[n];//dizideki elemanların frekanlarını depolamak için
-        for (int i=0;i< dizi.length;i++) {
-            System.out.print((i + 1) + ". elemanını girin: ");
-            dizi[i] = reader.nextInt();
-        }
-        int kontrol = -1;//tekrar aynı elemanın sayılmaması için
+        // Normal bir hesap açalım
+        System.out.println("--- Normal Hesap ---");
+        BankAccount hesaba1 = new BankAccount("Ahmet Yılmaz", 1000);
+        hesaba1.deposit(500);
+        hesaba1.withdraw(200);
+        hesaba1.showInfo();
 
-        for(int i = 0; i < dizi.length; i++){//diziden öğe seçmek için
-            int sayac = 1;
-            for(int j = i+1; j < dizi.length; j++){//dizinin geri kalanı ile karşılaştırmak için
-                if(dizi[i] == dizi[j]){
-                    sayac++;
-                    fr[j] = kontrol;//i elemanını değil ondan sonraki elemanı -1 e eşitliyor
-                    //bu sayede i değeri -1 eşitlenmiyor ve ekrana yazdırılıyor
-                }
-            }
-            if(fr[i] != kontrol)
-                fr[i] = sayac;
-        }
-        for(int i = 0; i < fr.length; i++){
-            if(fr[i] != kontrol)
-                System.out.println(dizi[i] + " elemanı-frekansı " + fr[i]);
-        }
+        // Vadeli (Faizli) hesap açalım
+        System.out.println("--- Vadeli Hesap ---");
+        SavingsAccount vadeliHesap = new SavingsAccount("Ayşe Demir", 2000, 5); // %5 faiz
+        vadeliHesap.addInterest(); // Faizi işlet
+        vadeliHesap.withdraw(3000); // Yetersiz bakiye denemesi
+        vadeliHesap.showInfo();
     }
 }
